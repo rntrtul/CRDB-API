14de7aef9a04c43fdb7999784d9f77888609ecff
Rntrtul <Sistthelord@gmail.com>
2020-06-19 18:50:18 -0400

    fix model order and add rolls as app

--- rolls/models.py
+++ rolls/models.py
@@ -1,12 +1,12 @@
 from django.db import models
 
 # Create your models here.
+class RollType(models.Model):
+  name = models.CharField(max_length=100)
+
 class Rolls(models.Model):
   time = models.IntegerField(default=0)
   naturalValue = models.IntegerField(default=0)
   finalValue = models.IntegerField(default=0)
   notes = models.CharField(max_length=200)
   rollType = models.ForeignKey(RollType,on_delete=models.CASCADE )
-
-class RollType(models.Model):
-  name = models.CharField(max_length=100)
