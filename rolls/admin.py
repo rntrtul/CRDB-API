cd288fef8a41979334e2fe1e53d31ada163a25b7
Rntrtul <Sistthelord@gmail.com>
2020-06-20 17:38:20 -0400

    added some admin and stlye

--- rolls/admin.py
+++ rolls/admin.py
@@ -1,5 +1,16 @@
 from django.contrib import admin
-from .models import RollType
+from .models import RollType, Rolls
 # Register your models here.
 
+class RollTypeInLine(admin.StackedInline):
+  model = RollType
+  extra = 1
+
+class RollsAdmin(admin.ModelAdmin):
+  fieldsets = [
+    (None, {'fields': ['notes', 'timeStamp']}),
+    ('Die Roll', {'fields': ['finalValue', 'naturalValue', 'rollType']}),
+  ]
+
 admin.site.register(RollType)
+admin.site.register(Rolls, RollsAdmin)
