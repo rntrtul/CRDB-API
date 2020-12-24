f84dffd98c35190448d64d32622857a27349be84
Rntrtul <Sistthelord@gmail.com>
2020-12-23 20:01:10 -0500

    remove page limit

--- CritRoleDB/settings.py
+++ CritRoleDB/settings.py
@@ -166,7 +166,6 @@ REST_FRAMEWORK = {
     ),
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
     'DEFAULT_FILTER_BACKENDS': ['django_filters.rest_framework.DjangoFilterBackend'],
-    'PAGE_SIZE': 250
 }
 
 SILKY_PYTHON_PROFILER = DEBUG
